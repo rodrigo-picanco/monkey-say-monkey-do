ce03fb20fd6c158464418d71dc95201d8898b196
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-09-20 15:24:17 +0200

    evaluate strings

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -24,19 +24,19 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return nativeBoolToBooleanObject(node.Value)
 	case *ast.PrefixExpression:
 		right := Eval(node.Right, env)
-                if isError(right) {
-                  return right
-                }
+		if isError(right) {
+			return right
+		}
 		return evalPrefixExpression(node.Operator, right)
 	case *ast.InfixExpression:
 		left := Eval(node.Left, env)
-                if isError(left) {
-                  return left
-                }
+		if isError(left) {
+			return left
+		}
 		right := Eval(node.Right, env)
-                if isError(right) {
-                  return right
-                }
+		if isError(right) {
+			return right
+		}
 		return evalInfixExpression(node.Operator, left, right)
 	case *ast.BlockStatement:
 		return evalBlockStatement(node, env)
@@ -44,32 +44,34 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalIfExpression(node, env)
 	case *ast.ReturnStatement:
 		val := Eval(node.ReturnValue, env)
-                if isError(val) {
-                  return val
-                }
+		if isError(val) {
+			return val
+		}
 		return &object.ReturnValue{Value: val}
-        case *ast.LetStatement:
-          val := Eval(node.Value, env)
-          if isError(val) {
-            return val
-          }
-          env.Set(node.Name.Value, val)
-        case *ast.Identifier:
-          return evalIdentifier(node, env)
-        case *ast.FunctionLiteral: 
-          params := node.Parameters
-          body := node.Body
-          return &object.Function{Parameters: params, Env: env, Body: body}
-        case *ast.CallExpression:
-          function := Eval(node.Function, env)
-          if isError(function) {
-            return function
-          }
-          args := evalExpressions(node.Arguments, env)
-          if len(args) == 1 && isError(args[0]) {
-            return args[0]
-          }
-          return applyFunction(function, args)
+	case *ast.LetStatement:
+		val := Eval(node.Value, env)
+		if isError(val) {
+			return val
+		}
+		env.Set(node.Name.Value, val)
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
 	}
 	return nil
 }
@@ -145,6 +147,8 @@ func evalInfixExpression(operator string, left object.Object, right object.Objec
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case operator == "==":
 		result := left == right
 		return nativeBoolToBooleanObject(result)
@@ -184,9 +188,9 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 
 func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Object {
 	condition := Eval(ie.Condition, env)
-        if isError(condition) {
-          return condition
-        }
+	if isError(condition) {
+		return condition
+	}
 
 	if isTruthy(condition) {
 		return Eval(ie.Consequence, env)
@@ -213,59 +217,72 @@ func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
 }
 func isError(obj object.Object) bool {
-  if obj != nil {
-    return obj.Type() == object.ERROR_OBJ
-  }
-  return false
+	if obj != nil {
+		return obj.Type() == object.ERROR_OBJ
+	}
+	return false
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-  val, ok := env.Get(node.Value)
-  if !ok {
-    return newError("identifier not found: " + node.Value)
-  }
+	val, ok := env.Get(node.Value)
+	if !ok {
+		return newError("identifier not found: " + node.Value)
+	}
 
-  return val
+	return val
 }
 
-
 func evalExpressions(
-  exps []ast.Expression,
-  env *object.Environment,
+	exps []ast.Expression,
+	env *object.Environment,
 ) []object.Object {
-  var result []object.Object
-  for _, e := range exps {
-    evaluated := Eval(e, env)
-    if isError(evaluated) {
-      return []object.Object{evaluated}
-    }
-    result = append(result, evaluated)
-  }
-  return result
+	var result []object.Object
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-  function, ok := fn.(*object.Function)
-  if !ok {
-    return newError("not a function: %s", fn.Type())
-  }
-  extendedEnv := extendFunctionEnv(function, args)
-  evaluated := Eval(function.Body, extendedEnv)
-  return unwrapReturnValue(evaluated)
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
 }
 func extendFunctionEnv(
-  fn *object.Function,
-  args []object.Object,
+	fn *object.Function,
+	args []object.Object,
 ) *object.Environment {
-  env := object.NewEnclosedEnvironment(fn.Env)
-  for paramIdx, param := range fn.Parameters {
-    env.Set(param.Value, args[paramIdx])
-  }
-  return env
+	env := object.NewEnclosedEnvironment(fn.Env)
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+	return env
 }
 func unwrapReturnValue(obj object.Object) object.Object {
-  if ReturnValue, ok := obj.(*object.ReturnValue); ok {
-    return ReturnValue.Value
-  }
-  return obj
+	if ReturnValue, ok := obj.(*object.ReturnValue); ok {
+		return ReturnValue.Value
+	}
+	return obj
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{
+		Value: leftVal + rightVal,
+	}
 }
