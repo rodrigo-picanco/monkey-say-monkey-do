690a0f84966f39bed9b948e2e3bef7b998b4804b
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-10-30 22:43:57 +0100

    add op

--- code/code.go
+++ code/code.go
@@ -1,6 +1,7 @@
 package code
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 )
@@ -11,7 +12,8 @@ type Definition struct {
 }
 
 var definitions = map[Opcode]*Definition{
-	OpConstant: {"Opconstante", []int{2}},
+	OpConstant: {"OpConstant", []int{2}},
+        OpAdd: {"OpAdd", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -26,7 +28,10 @@ type Instructions []byte
 
 type Opcode byte
 
-const OpConstant Opcode = iota
+const ( 
+    OpConstant Opcode = iota
+    OpAdd
+)
 
 func Make(op Opcode, operands ...int) []byte {
 	def, ok := definitions[op]
@@ -46,7 +51,53 @@ func Make(op Opcode, operands ...int) []byte {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
 		}
-                offset += width
+		offset += width
 	}
 	return instruction
+}
+
+func (ins Instructions) String() string {
+	var out bytes.Buffer
+	i := 0
+	for i < len(ins) {
+		def, err := Lookup(ins[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+		operands, read := ReadOperands(def, ins[i+1:])
+		fmt.Fprintf(&out, "%04d %s\n", i, ins.fmtInstruction(def, operands))
+		i += 1 + read
+	}
+
+	return out.String()
+}
+func (ins Instructions) fmtInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand len %d does not match defined %d\n", len(operands), operandCount)
+	}
+	switch operandCount {
+        case 0:
+            return def.Name
+	case 1:
+		return fmt.Sprintf("%s %d", def.Name, operands[0])
+	}
+	return fmt.Sprintf("ERROR: unhandled operandCount for %s\n", def.Name)
+}
+
+func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
+	operands := make([]int, len(def.OperandWidths))
+	offset := 0
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2:
+			operands[i] = int(ReadUint16(ins[offset:]))
+		}
+		offset += width
+	}
+	return operands, offset
+}
+func ReadUint16(ins Instructions) uint16 {
+	return binary.BigEndian.Uint16(ins)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"monkey/evaluator"
+	"monkey/compiler"
+	"monkey/vm"
 	"monkey/lexer"
-	"monkey/object"
 	"monkey/parser"
 )
 
@@ -14,7 +14,6 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-        env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -25,25 +24,36 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-                p := parser.New(l)
+		p := parser.New(l)
 
-                program := p.ParseProgram()
-                if len(p.Errors()) != 0 {
-                  printParserErrors(out, p.Errors())
-                  continue
-                }
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
 
-                evaluated := evaluator.Eval(program, env)
-                if evaluated != nil {
-                  io.WriteString(out, evaluated.Inspect())
-                  io.WriteString(out, "\n")
-                }
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+                        continue
+		}
+
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+                        continue
+		}
+                stackTop := machine.StackTop()
+                io.WriteString(out, stackTop.Inspect())
+                io.WriteString(out, "\n")
 
 	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-  for _, msg := range errors {
-    io.WriteString(out, "\t"+msg+"\n")
-  }
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
