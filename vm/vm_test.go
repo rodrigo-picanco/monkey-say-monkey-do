c77271bb590b073403a649c6302ad6602cfea607
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-11-30 15:58:00 +0100

    run empty functions with vm (218)

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -281,6 +281,53 @@ func TestCallingFunctionsWithoutArguments(t *testing.T)  {
                         `,
                         expected: 15,
                 },
+                {
+                        input: `
+                                let one = fn() { 1; };
+                                let two = fn() { 2; };
+                                one() + two()
+                        `,
+                        expected: 3,
+                },
+                {
+                        input: `
+                                let a = fn() { 1 };
+                                let b = fn() { a() + 1 };
+                                let c = fn() { b() + 1 };
+                                c();
+                        `,
+                        expected: 3,
+                },
+                {
+                        input: `
+                                let earlyExit = fn() { return 99; return 100; }
+                                earlyExit();
+                        `,
+                        expected: 99,
+                },
+
+        }
+        runVmTests(t, tests)
+}
+
+func TestFunctionsWithoutReturnValue(t *testing.T)  {
+        tests :=[]vmTestCase{
+                {
+                        input: `
+                                let noReturn = fn() { };
+                                noReturn();
+                        `,
+                        expected: Null,
+                },
+                {
+                        input: `
+                                let noReturn = fn() { };
+                                let noReturnTwo = fn () { noReturn(); };
+                                noReturn()
+                                noReturnTwo();
+                        `,
+                        expected: Null,
+                },
         }
         runVmTests(t, tests)
 }
