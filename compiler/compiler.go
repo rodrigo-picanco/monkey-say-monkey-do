c796aa1d40bc7501e3edc3e415afe0d2af171edb
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-12-01 18:23:09 +0100

    emit local constants opcodes on compiler (239)

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -161,13 +161,21 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return err
 		}
 		symbol := c.symbolTable.Define(node.Name.Value)
-		c.emit(code.OpSetGlobal, symbol.Index)
+                if symbol.Scope == GlobalScope {
+                        c.emit(code.OpSetGlobal, symbol.Index)
+                } else {
+                        c.emit(code.OpSetLocal, symbol.Index)
+                }
 	case *ast.Identifier:
 		symbol, ok := c.symbolTable.Resolve(node.Value)
 		if !ok {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
-		c.emit(code.OpGetGlobal, symbol.Index)
+                if symbol.Scope == GlobalScope {
+                        c.emit(code.OpGetGlobal, symbol.Index)
+                } else {
+                        c.emit(code.OpGetLocal, symbol.Index)
+                }
 	case *ast.StringLiteral:
 		str := &object.String{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(str))
